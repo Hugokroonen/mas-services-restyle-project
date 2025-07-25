e05fda2fc8123156421d5b3e63bb49a41f6aee92
Hugo Kroonen <hkroonen@enjins.com>
2025-07-25 17:57:25 +0200

    Fix subpages

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+(function() {
+  const { search } = window.location;
+  const params = new URLSearchParams(search);
+  const path = params.get('p');
+
+  if (path) {
+    window.history.replaceState(null, '', path);
+  }
+})();
+
 createRoot(document.getElementById("root")!).render(<App />);
